0fc531965d0b4e1de418c78deb0d07a618942b7b
cherepanov92 <cherepanov92@gmail.com>
2020-12-18 12:39:25 +0500

    [*] Установка firebase пакетом. Запрос данных коллекции.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,35 @@ import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbPgBetggB8eOnJuc4ErNAuOddB0ddKIw",
+  authDomain: "kanban-eec37.firebaseapp.com",
+  databaseURL: "https://kanban-eec37-default-rtdb.firebaseio.com",
+  projectId: "kanban-eec37",
+  storageBucket: "kanban-eec37.appspot.com",
+  messagingSenderId: "54488392624",
+  appId: "1:54488392624:web:5f4bb0845f5d1ed5792c75",
+  measurementId: "G-X6M6QM6N8R"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
+
+const db = firebase.firestore();
+db.collection("desks").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+  });
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
