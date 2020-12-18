0415ea8f0233f9c2b34ad419d6345c733a6df7ee
cherepanov92 <cherepanov92@gmail.com>
2020-12-19 00:48:26 +0500

    [*] Отчистка проекта от демо Базовое применение view

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import React, { useState } from 'react';
+// import bridge from '@vkontakte/vk-bridge';
+import { View, Panel,Button  } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+const panel = {
+	desks: 'desks',
+	columns: 'columns'
+}
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
-
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+	const [activePanel, setActivePanel] = useState(panel.desks)
 
 	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
+		<View activePanel={activePanel}>
+
+			<Panel id={panel.desks}>
+				<div>Панель досок</div>
+				<Button onClick={() => setActivePanel(panel.columns)}>Перейти к колонкам</Button>
+			</Panel>
+
+			<Panel id={panel.columns}>
+				<div>Панель колонок</div>
+				<Button onClick={() => setActivePanel(panel.desks)}>Перейти к доскам</Button>
+			</Panel>
+
 		</View>
 	);
-}
+};
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "core-js/features/map";
 import "core-js/features/set";
 import React from "react";
 import ReactDOM from "react-dom";
-import bridge from "@vkontakte/vk-bridge";
+// import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 import firebase from 'firebase/app';
@@ -26,16 +26,11 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 // Init VK  Mini App
-bridge.send("VKWebAppInit");
+// bridge.send("VKWebAppInit");
 
-const db = firebase.firestore();
-db.collection("desks").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-  });
-});
+// const db = firebase.firestore();
+// db.collection("desks").get().then((querySnapshot) => {
+//   querySnapshot.forEach(doc => console.log(doc.id, doc.data()));
+// });
 
 ReactDOM.render(<App />, document.getElementById("root"));
-if (process.env.NODE_ENV === "development") {
-  import("./eruda").then(({ default: eruda }) => {}); //runtime download
-}
