67ca2f7971ac677167f8637b5c2c05d21a037d84
cherepanov92 <cherepanov92@gmail.com>
2021-01-03 01:10:42 +0500

    [*] Получение и удаление карточек с бека

---
+++ src/components/Cards.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import ColumnCard from './ColumnCard';
+import { CardGrid } from '@vkontakte/vkui';
+import firebase from 'firebase';
+
+const Cards = () => {
+  const [cards, setCards] = useState([]);
+  const addCard = (card) => setCards([...cards, card]);
+  const removeCard = (removeId) => setCards(cards.filter(({id}) => {return id !== removeId}));
+
+  // Запрос данных о карточках
+  useEffect(() => {
+    const db = firebase.firestore();
+    
+    db.collection("cards").get().then((querySnapshot) => {
+      const cards = [];
+      
+      querySnapshot.forEach((doc) => {
+        const { columnId, name } = doc.data();
+
+        cards.push({
+          id: doc.id, 
+          columnId,
+          name,
+        });
+      });
+
+      setCards(cards);
+    });
+  }, []);
+
+  return (
+    <CardGrid>
+      {cards.map(({ id, name }) => <ColumnCard key={id} id={id} onDelete={removeCard}>{name}</ColumnCard>)}
+    </CardGrid>
+  );
+};
+
+export default Cards;
