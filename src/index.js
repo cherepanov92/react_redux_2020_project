b1c30def635c223f7f319485bac4c03a5d891b90
cherepanov92 <cherepanov92@gmail.com>
2021-01-08 12:00:15 +0500

    [*] Вынес инициализацию бекенда в отдельный модуль

--- src/index.js
+++ src/index.js
@@ -4,28 +4,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import bridge from "@vkontakte/vk-bridge";
 import App from "./components/App/App";
-
-import firebase from 'firebase/app';
-import 'firebase/analytics';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAbPgBetggB8eOnJuc4ErNAuOddB0ddKIw",
-  authDomain: "kanban-eec37.firebaseapp.com",
-  databaseURL: "https://kanban-eec37-default-rtdb.firebaseio.com",
-  projectId: "kanban-eec37",
-  storageBucket: "kanban-eec37.appspot.com",
-  messagingSenderId: "54488392624",
-  appId: "1:54488392624:web:5f4bb0845f5d1ed5792c75",
-  measurementId: "G-X6M6QM6N8R"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+import * as backend from './models/firebase';
 
 // Init VK  Mini App
 // bridge.send("VKWebAppInit");
+
+backend.initialize();
 
 ReactDOM.render(<App />, document.getElementById("root"));
