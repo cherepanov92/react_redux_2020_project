2ab45ee6249d826dd47fc78e464f0215f327b368
cherepanov92 <cherepanov92@gmail.com>
2021-01-09 01:45:50 +0500

    [*] Вынес состояния App в кастомные хуки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,24 +12,15 @@ const panel = {
 }
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState(panel.desks);
-	const [activeDesk, setActiveDesk] = useState({});
-	const goToColumns = (deskId) => {
-		setActiveDesk(desks.find(({id}) => id === deskId));
-		setActivePanel(panel.columns);
-	}
-	const goToDesks = () => setActivePanel(panel.desks);
-
 	// Доски
-	const [desks, setDesks] = useState([]);
-  const addDesk = (desk) => setDesks([...desks, desk]);
-  const removeDesk = (removeId) => setDesks(desks.filter(({id}) => id !== removeId));
-
+	const { desks, setDesks, addDesk, removeDesk } = useDesksState();
+	
 	// Колонки
-  const [columns, setColumns] = useState([]);
-  const addColumn = (column) => setColumns([...columns, column]);
-  const removeColumn = (removeId) => setColumns(columns.filter(({id}) => id !== removeId));
-
+	const { columns, setColumns, addColumn, removeColumn } = useColumnsState();
+	
+	// Панели
+	const { activePanel, activeDesk, goToColumns, goToDesks } = useAppState(desks);
+	
 	return (
 		<View activePanel={activePanel}>
 			<Panel id={panel.desks} separator={false}>
@@ -56,6 +47,34 @@ const App = () => {
 			</Panel>
 		</View>
 	);
+};
+
+const useDesksState = () => {
+	const [desks, setDesks] = useState([]);
+  const addDesk = (desk) => setDesks([...desks, desk]);
+  const removeDesk = (removeId) => setDesks(desks.filter(({id}) => id !== removeId));
+	
+	return { desks, setDesks, addDesk, removeDesk };
+};
+
+const useColumnsState = () => {
+	const [columns, setColumns] = useState([]);
+  const addColumn = (column) => setColumns([...columns, column]);
+  const removeColumn = (removeId) => setColumns(columns.filter(({id}) => id !== removeId));
+
+	return { columns, setColumns, addColumn, removeColumn };
+};
+
+const useAppState = (desks) => {
+	const [activePanel, setActivePanel] = useState(panel.desks);
+	const [activeDesk, setActiveDesk] = useState({});
+	const goToColumns = (deskId) => {
+		setActiveDesk(desks.find(({id}) => id === deskId));
+		setActivePanel(panel.columns);
+	}
+	const goToDesks = () => setActivePanel(panel.desks);
+
+	return { activePanel, activeDesk, goToColumns, goToDesks };
 };
 
 export default App;
